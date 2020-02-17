5d7985f86e86f7963a4a7b6d5c596087398b113c
mcbiggos@gmail.com <michal@kozlovv.ski>
2020-02-17 12:58:10 +0100

    add jak to działa section

--- front-end/src/views/Index.tsx
+++ front-end/src/views/Index.tsx
@@ -1,6 +1,6 @@
-import { Button, Typography, Tooltip } from "antd";
+import { Button, Typography, Tooltip, Modal } from "antd";
 import Title from "antd/lib/typography/Title";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 
 import { ViewContext } from "../components/ViewManager";
 import WelcomeText from "../components/WelcomeText";
@@ -9,6 +9,7 @@ interface Props {}
 
 const Index: React.FC<Props> = () => {
   const { changeView } = useContext(ViewContext);
+  const [showHowItWorks, setShowHowItWorks] = useState(false);
 
   return (
     <div className="wrapper">
@@ -24,9 +25,38 @@ const Index: React.FC<Props> = () => {
         </a>
         , a my zajmiemy się resztą!
       </Typography.Paragraph>
+      <Button
+        style={{ paddingLeft: 0 }}
+        type="link"
+        onClick={e => setShowHowItWorks(prev => !prev)}
+      >
+        Jak to działa?
+      </Button>
       <Button type="primary" onClick={e => changeView(1)}>
         Zaczynamy!
       </Button>
+      <Modal
+        visible={showHowItWorks}
+        title="Jak to działa?"
+        okText="Zamknij"
+        footer={
+          <Button onClick={e => setShowHowItWorks(false)}>Zamknij</Button>
+        }
+        onCancel={() => setShowHowItWorks(false)}
+      >
+        <Typography.Paragraph>
+          Na podstawie sygnatur wyszukujemy Twoje przedmioty w naszej bazie
+          utworzonej z plików harmonogramu ze{" "}
+          <a
+            href="http://administracja.sgh.waw.pl/pl/DSL/harmonogramy_zajec/Strony/default.aspx"
+            target="_blank"
+          >
+            strony Dziekanatu Studium Licencjakiego
+          </a>
+          . Po znalezieniu zajęć tworzymy w Twoim koncie Google nowy kalendarz
+          "SGH - zajęcia" i dodajemy do niego odpowiednie wydarzenia. I gotowe!
+        </Typography.Paragraph>
+      </Modal>
     </div>
   );
 };
