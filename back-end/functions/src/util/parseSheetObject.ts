25598d52c5e90efbd895653d8b1c4846a069b3f2
mcbiggos@gmail.com <michal@kozlovv.ski>
2020-02-18 09:10:51 +0100

    update xls row parsing

--- back-end/functions/src/util/parseSheetObject.ts
+++ back-end/functions/src/util/parseSheetObject.ts
@@ -7,6 +7,13 @@ const parseSheetObject = (data: SheetLecture, type: string): LecturesEntry => {
   const lecturer = data.Prowadzący.replace(/[-\d\s]{2,}/, "");
   const dates = data["Daty zajęć (dd-mm-rr)"].match(/\d{2}-\d{2}-\d{2}/g);
 
+  const formMap = {
+    ćwiczenia: "C",
+    wykład: "W"
+  };
+  const formKey = data.Forma.toLowerCase() as keyof typeof formMap;
+  const form: string = formMap[formKey];
+
   if (przedmiot_split.length !== 2) {
     console.error(data.Przedmiot, przedmiot_split);
   }
@@ -16,15 +23,16 @@ const parseSheetObject = (data: SheetLecture, type: string): LecturesEntry => {
   }
 
   const res = {
-    signature: przedmiot_split[0],
-    name: przedmiot_split[1],
-    start_time: data.Poczatek,
-    end_time: data.Koniec,
+    signature: przedmiot_split[0] || "",
+    name: przedmiot_split[1] || "",
+    start_time: data.Poczatek || "",
+    end_time: data.Koniec || "",
     dates: dates || [],
-    form: data.Forma,
-    group: String(data["Numer grupy"]),
-    lecturer,
-    type
+    form: form || "",
+    group: String(data["Numer grupy"]) || "",
+    lecturer: lecturer || "",
+    type: type || "",
+    place: data["Budynek i sala"] || ""
   };
 
   return res;
