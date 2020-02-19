d431cc03726d3a7e85deaf0310428e627d7954aa
mcbiggos@gmail.com <michal@kozlovv.ski>
2020-02-19 23:22:50 +0100

    add basic mobile styling

--- front-end/src/layout/Layout.tsx
+++ front-end/src/layout/Layout.tsx
@@ -7,14 +7,12 @@ interface Props {}
 
 const Layout: React.FC<Props> = ({ children }) => {
   return (
-    <Row type="flex" className="full-height">
-      <Col span={16} className="app-panel">
-        {children}
-      </Col>
-      <Col span={8} className="user-panel">
+    <div className="full-height">
+      <div className="app-panel">{children}</div>
+      <div className="user-panel">
         <UserPanel />
-      </Col>
-    </Row>
+      </div>
+    </div>
   );
 };
 
