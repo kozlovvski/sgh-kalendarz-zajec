6b0176422174565e8ef9e69ed2b6657c942e233b
mcbiggos@gmail.com <michal@kozlovv.ski>
2020-02-20 12:11:47 +0100

    add scrollbar styling

--- front-end/src/layout/Layout.tsx
+++ front-end/src/layout/Layout.tsx
@@ -1,16 +1,33 @@
-import { Col, Row } from "antd";
-import React from "react";
+import { Col, Row, Icon } from "antd";
+import React, { useState } from "react";
 
 import UserPanel from "./UserPanel";
 
 interface Props {}
 
 const Layout: React.FC<Props> = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="full-height">
-      <div className="app-panel">{children}</div>
+    <div className="app" data-menu-open={isOpen}>
+      <div className="app-panel">
+        {children}
+        <button
+          className="user-panel-button user-panel-button--open"
+          onClick={e => setIsOpen(true)}
+        >
+          Logowanie <Icon type="arrow-right" />
+        </button>
+      </div>
       <div className="user-panel">
         <UserPanel />
+        <button
+          className="user-panel-button user-panel-button--close"
+          onClick={e => setIsOpen(false)}
+        >
+          <Icon type="arrow-left" />
+          Powrót
+        </button>
       </div>
     </div>
   );
